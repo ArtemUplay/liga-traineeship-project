2d819ff51397f60cee6d873a7e5470389ec2c8e2
Artem Pronin <98651512+ArtemUplay@users.noreply.github.com>
2023-11-15 12:07:21 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { App } from '../src/app/App';
-import { store } from './store/store';
+import { App } from 'src/app/App';
+import { store } from 'src/store/store';
 
 const container = document.getElementById('root');
 const root = createRoot(container as HTMLElement);
